aa8130aec424642f3c1afacc22814cedf8d89210
NavidNasyry <navidnasyry@gmail.com>
2022-08-26 12:46:59 +0430

    add runner bashscript

---
+++ run.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+pip3 install -r requirements.txt
+
+rm -rf ./Release
+
+mkdir ./Release
+
+cd ./Release
+
+cmake -DCMAKE_BUILD_TYPE=Release ..
+
+make
+
+cp -r ../after_create_release/* ./examples
+
+python3 ./examples/get_input/app.py 
+
+
